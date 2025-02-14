bd4c482fb4d94f87405bc153e52cc689f2f3e18b
Chris Dzombak <chris@dzombak.com>
2025-02-14 13:38:34 -0500

    fix display of tables that don't scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
 (function () {
     pagination(false);
 })();
+
+function isOverflownX(element) {
+    return element.scrollWidth > element.clientWidth;
+}
+
+window.addEventListener('load', function () {
+    document.querySelectorAll('.gh-content table:not(.gist table)').forEach((element) => {
+        if (!isOverflownX(element)) {
+            element.style.background = 'none';
+        }
+    });
+});
