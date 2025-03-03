2d2358d00b3068ff3b2ecb697219128b4bd137fe
Chris Dzombak <chris@dzombak.com>
2025-03-03 11:21:19 -0500

    cache homepage feeds properly

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,182 +11,205 @@ window.addEventListener("load", function () {
 });
 
 window.addEventListener("DOMContentLoaded", function () {
-  // TODO(cdzombak): check cache & expiration ts
+  const populateMicroblog = function () {
+    const microblogItems = JSON.parse(window.localStorage.getItem("cdz_microblog_feed")).items;
+    const microblogFeed = document.getElementById("-cdz-microblog-feed");
 
-  let parser = new RSSParser();
-  parser.parseURL("https://www2025.dzombak.com/rss/microblog.rss", function (err, feed) {
-    if (err) {
-      console.error(err);
-      return;
-    }
+    microblogItems.forEach(function (item) {
+      const article = document.createElement("article");
+      article.setAttribute("class", "gh-card post");
 
-    const feedItems = feed.items;
-    feedItems.sort(function (a, b) {
-      return new Date(Date.parse(b.pubDate)) - new Date(Date.parse(a.pubDate));
+      const a = document.createElement("a");
+      a.setAttribute("class", "gh-card-link");
+      a.setAttribute("href", item.link);
+      a.setAttribute("target", "_blank");
+      article.appendChild(a);
+
+      const containerDiv = document.createElement("div");
+      containerDiv.setAttribute("class", "gh-card-excerpt cdz-microblog-container");
+      a.appendChild(containerDiv);
+
+      const contentDiv = document.createElement("div");
+      contentDiv.setAttribute("class", "cdz-microblog-content");
+      containerDiv.appendChild(contentDiv);
+
+      const textContentDiv = document.createElement("div");
+      textContentDiv.setAttribute("class", "cdz-microblog-text-content");
+      textContentDiv.innerHTML = item.content;
+      contentDiv.appendChild(textContentDiv);
+
+      if (item.encType) {
+        if (item.encType.startsWith("image/") && !!item.encURL) {
+          const img = document.createElement("img");
+          img.setAttribute("src", item.encURL);
+          contentDiv.appendChild(img);
+        } else {
+          console.warn("microblog item has unsuported enclosure type:", item);
+        }
+      }
+
+      const footer = document.createElement("footer");
+      footer.setAttribute("class", "gh-card-meta");
+      textContentDiv.appendChild(footer);
+
+      const itemTs = new Date(Date.parse(item.pubDate));
+      const time = document.createElement("time");
+      time.setAttribute("class", "gh-card-date");
+      time.setAttribute("datetime", item.pubDate);
+      time.appendChild(document.createTextNode(itemTs.toLocaleString()));
+      footer.appendChild(time);
+
+      microblogFeed.appendChild(article);
     });
+  };
 
-    var items = [];
-    feedItems.forEach(function (entry) {
-      if (items.length >= 3) {
+  const cached = window.localStorage.getItem("cdz_microblog_feed");
+  let refreshCache = true;
+  if (cached) {
+    const cacheExp = new Date(Date.parse(JSON.parse(cached).exp));
+    refreshCache = new Date().getTime() >= cacheExp.getTime();
+  }
+
+  if (refreshCache) {
+    const parser = new RSSParser();
+    parser.parseURL("https://www2025.dzombak.com/rss/microblog.rss", function (err, feed) {
+      if (err) {
+        console.error(err);
         return;
       }
 
-      var item = {
-        pubDate: new Date(Date.parse(entry.pubDate)),
-        link: entry.link,
-        content: entry.content.replaceAll("<br>", ""),
-      };
+      const feedItems = feed.items;
+      feedItems.sort(function (a, b) {
+        return new Date(Date.parse(b.pubDate)) - new Date(Date.parse(a.pubDate));
+      });
 
-      if (entry.enclosure) {
-        item.encType = entry.enclosure.type;
-        item.encURL = entry.enclosure.url;
-      }
+      var items = [];
+      feedItems.forEach(function (entry) {
+        if (items.length >= 3) {
+          return;
+        }
 
-      items.push(item);
+        var item = {
+          pubDate: new Date(Date.parse(entry.pubDate)),
+          link: entry.link,
+          content: entry.content.replaceAll("<br>", ""),
+        };
+
+        if (entry.enclosure) {
+          item.encType = entry.enclosure.type;
+          item.encURL = entry.enclosure.url;
+        }
+
+        items.push(item);
+      });
+
+      window.localStorage.setItem(
+        "cdz_microblog_feed",
+        JSON.stringify({
+          exp: new Date(new Date().getTime() + 5 * 60 * 1000),
+          items: items,
+        }),
+      );
+      populateMicroblog();
     });
-
-    window.localStorage.setItem(
-      "cdz_microblog_feed",
-      JSON.stringify({
-        exp: new Date(new Date().getTime() + 5 * 60 * 1000),
-        items: items,
-      }),
-    );
-  });
-
-  const microblogItems = JSON.parse(window.localStorage.getItem("cdz_microblog_feed")).items;
-  const microblogFeed = document.getElementById("-cdz-microblog-feed");
-
-  microblogItems.forEach(function (item) {
-    const article = document.createElement("article");
-    article.setAttribute("class", "gh-card post");
-
-    const a = document.createElement("a");
-    a.setAttribute("class", "gh-card-link");
-    a.setAttribute("href", item.link);
-    a.setAttribute("target", "_blank");
-    article.appendChild(a);
-
-    const containerDiv = document.createElement("div");
-    containerDiv.setAttribute("class", "gh-card-excerpt cdz-microblog-container");
-    a.appendChild(containerDiv);
-
-    const contentDiv = document.createElement("div");
-    contentDiv.setAttribute("class", "cdz-microblog-content");
-    containerDiv.appendChild(contentDiv);
-
-    const textContentDiv = document.createElement("div");
-    textContentDiv.setAttribute("class", "cdz-microblog-text-content");
-    textContentDiv.innerHTML = item.content;
-    contentDiv.appendChild(textContentDiv);
-
-    if (item.encType) {
-      if (item.encType.startsWith("image/") && !!item.encURL) {
-        const img = document.createElement("img");
-        img.setAttribute("src", item.encURL);
-        contentDiv.appendChild(img);
-      } else {
-        console.warn("microblog item has unsuported enclosure type:", item);
-      }
-    }
-
-    const footer = document.createElement("footer");
-    footer.setAttribute("class", "gh-card-meta");
-    textContentDiv.appendChild(footer);
-
-    const itemTs = new Date(Date.parse(item.pubDate));
-    const time = document.createElement("time");
-    time.setAttribute("class", "gh-card-date");
-    time.setAttribute("datetime", item.pubDate);
-    time.appendChild(document.createTextNode(itemTs.toLocaleString()));
-    footer.appendChild(time);
-
-    microblogFeed.appendChild(article);
-  });
+  } else {
+    populateMicroblog();
+  }
 });
 
 window.addEventListener("DOMContentLoaded", function () {
-  // TODO(cdzombak): check cache & expiration ts
+  const populatePictures = function () {
+    const pictureItems = JSON.parse(window.localStorage.getItem("cdz_pictures_feed")).items;
+    const picturesFeed = document.getElementById("-cdz-pictures-feed");
+    const bbItems = JSON.parse(window.localStorage.getItem("cdz_birdbuddy_feed")).items;
+    const bbFeed = document.getElementById("-cdz-birdbuddy-feed");
 
-  let parser = new RSSParser();
-  parser.parseURL("https://www2025.dzombak.com/rss/pictures.rss", function (err, feed) {
-    if (err) {
-      console.error(err);
-      return;
-    }
+    const addImage = function (el, item) {
+      const itemTs = new Date(Date.parse(item.pubDate));
+      const altTxt = item.title + " (Posted " + itemTs.toLocaleDateString() + " in “" + item.category + "”)";
 
-    const feedItems = feed.items;
-    feedItems.sort(function (a, b) {
-      return new Date(Date.parse(b.pubDate)) - new Date(Date.parse(a.pubDate));
+      const img = document.createElement("img");
+      img.setAttribute("src", item.encURL);
+      img.setAttribute("alt", altTxt);
+
+      const a = document.createElement("a");
+      a.setAttribute("class", "gh-card-link");
+      a.setAttribute("href", item.link);
+      a.setAttribute("target", "_blank");
+      a.setAttribute("title", altTxt);
+
+      a.appendChild(img);
+      el.appendChild(a);
+    };
+
+    pictureItems.forEach(function (item) {
+      addImage(picturesFeed, item);
     });
-
-    var items = [];
-    var birdbuddyItems = [];
-    feedItems.forEach(function (entry) {
-      var item = {
-        pubDate: new Date(Date.parse(entry.pubDate)),
-        link: entry.link,
-        title: entry.title,
-        category: null,
-        encType: entry.enclosure.type,
-        encURL: entry.enclosure.url,
-      };
-      if (entry.categories.length) {
-        item.category = entry.categories[0];
-      }
-
-      if (item.category && item.category === "Bird Buddy" && birdbuddyItems.length < 3) {
-        birdbuddyItems.push(item);
-      } else if (item.category !== "Bird Buddy" && items.length < 3) {
-        items.push(item);
-      }
+    bbItems.forEach(function (item) {
+      addImage(bbFeed, item);
     });
-
-    window.localStorage.setItem(
-      "cdz_pictures_feed",
-      JSON.stringify({
-        exp: new Date(new Date().getTime() + 15 * 60 * 1000),
-        items: items,
-      }),
-    );
-    window.localStorage.setItem(
-      "cdz_birdbuddy_feed",
-      JSON.stringify({
-        exp: new Date(new Date().getTime() + 15 * 60 * 1000),
-        items: birdbuddyItems,
-      }),
-    );
-  });
-
-  const pictureItems = JSON.parse(window.localStorage.getItem("cdz_pictures_feed")).items;
-  const picturesFeed = document.getElementById("-cdz-pictures-feed");
-  const bbItems = JSON.parse(window.localStorage.getItem("cdz_birdbuddy_feed")).items;
-  const bbFeed = document.getElementById("-cdz-birdbuddy-feed");
-
-  const addImage = function (el, item) {
-    const itemTs = new Date(Date.parse(item.pubDate));
-    const altTxt = item.title + " (Posted " + itemTs.toLocaleDateString() + " in “" + item.category + "”)";
-
-    const img = document.createElement("img");
-    img.setAttribute("src", item.encURL);
-    img.setAttribute("alt", altTxt);
-
-    const a = document.createElement("a");
-    a.setAttribute("class", "gh-card-link");
-    a.setAttribute("href", item.link);
-    a.setAttribute("target", "_blank");
-    a.setAttribute("title", altTxt);
-
-    a.appendChild(img);
-    el.appendChild(a);
   };
 
-  pictureItems.forEach(function (item) {
-    addImage(picturesFeed, item);
-  });
-  bbItems.forEach(function (item) {
-    addImage(bbFeed, item);
-  });
+  const cached = window.localStorage.getItem("cdz_pictures_feed");
+  let refreshCache = true;
+  if (cached) {
+    const cacheExp = new Date(Date.parse(JSON.parse(cached).exp));
+    refreshCache = new Date().getTime() >= cacheExp.getTime();
+  }
+
+  if (refreshCache) {
+    const parser = new RSSParser();
+    parser.parseURL("https://www2025.dzombak.com/rss/pictures.rss", function (err, feed) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const feedItems = feed.items;
+      feedItems.sort(function (a, b) {
+        return new Date(Date.parse(b.pubDate)) - new Date(Date.parse(a.pubDate));
+      });
+
+      var items = [];
+      var birdbuddyItems = [];
+      feedItems.forEach(function (entry) {
+        var item = {
+          pubDate: new Date(Date.parse(entry.pubDate)),
+          link: entry.link,
+          title: entry.title,
+          category: null,
+          encType: entry.enclosure.type,
+          encURL: entry.enclosure.url,
+        };
+        if (entry.categories.length) {
+          item.category = entry.categories[0];
+        }
+
+        if (item.category && item.category === "Bird Buddy" && birdbuddyItems.length < 3) {
+          birdbuddyItems.push(item);
+        } else if (item.category !== "Bird Buddy" && items.length < 3) {
+          items.push(item);
+        }
+      });
+
+      window.localStorage.setItem(
+        "cdz_pictures_feed",
+        JSON.stringify({
+          exp: new Date(new Date().getTime() + 15 * 60 * 1000),
+          items: items,
+        }),
+      );
+      window.localStorage.setItem(
+        "cdz_birdbuddy_feed",
+        JSON.stringify({
+          items: birdbuddyItems,
+        }),
+      );
+      populatePictures();
+    });
+  } else {
+    populatePictures();
+  }
 });
 
 (function () {
