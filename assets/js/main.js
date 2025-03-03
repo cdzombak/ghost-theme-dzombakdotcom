c3fe57df9dc969d4a0a45f537bd2f6b2aaef4fe1
Chris Dzombak <chris@dzombak.com>
2025-03-03 11:50:56 -0500

    add header links

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,20 @@ window.addEventListener("load", function () {
       element.style.background = "none";
     }
   });
+
+  document.querySelectorAll(".gh-content h2").forEach((element) => {
+    const aCont = document.createElement("span");
+    aCont.setAttribute("class", "cdz-content-header-link-container");
+
+    const a = document.createElement("a");
+    a.setAttribute("class", "cdz-content-header-link");
+    a.setAttribute("href", "#" + element.getAttribute("id"));
+    a.innerHTML =
+      "<svg viewBox='0 0 16 16' version='1.1' width='16' height='16'><path fill-rule='evenodd' d='M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z'></path></svg>";
+    aCont.appendChild(a);
+
+    element.prepend(aCont);
+  });
 });
 
 window.addEventListener("DOMContentLoaded", function () {
