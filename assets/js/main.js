1b8636602782b343da874dd957a7d2e86ede3e1a
Chris Dzombak <chris@dzombak.com>
2025-06-03 11:17:40 -0400

    mastodon link style

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,6 +120,7 @@ window.addEventListener("DOMContentLoaded", function () {
           item.encURL = entry.enclosure.url;
         } else if (entry['media:content'] && entry['media:content'].length > 0) {
           const mediaContent = entry['media:content'][0];
+          console.log(mediaContent);
           item.encType = mediaContent.type;
           item.encURL = mediaContent.url;
         }
